c77a9c8d154fb9a3ff256a7fbee3b47f5a9e11fd
Hicham Benkirane <hi.benkirane@gmail.com>
2022-04-30 03:00:04 +0200

    fix build (netlify)

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -fsSL https://deno.land/x/install/install.sh | sh
 export PATH="/opt/buildhome/.deno/bin:$PATH"
-deno run --allow-env -allow-net allow-read src/mod.ts
+deno run -A Drakefile.ts start
 exit 0
